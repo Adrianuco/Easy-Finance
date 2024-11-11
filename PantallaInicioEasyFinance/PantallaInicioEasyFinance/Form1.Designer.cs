bafdcfe825cf7bd9656147d09461f9f0f2ea4c6b
Juan Carlos Castellón Rivera <168575983+JuanCastellon-UAM-IngES@users.noreply.github.com>
2024-11-11 10:39:26 -0600

    Add files via upload

---
+++ PantallaInicioEasyFinance/PantallaInicioEasyFinance/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace PantallaInicioEasyFinance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblRegistro = new System.Windows.Forms.Label();
+            this.lblDatos1 = new System.Windows.Forms.Label();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.lblCategoría = new System.Windows.Forms.Label();
+            this.lblDatos2 = new System.Windows.Forms.Label();
+            this.btnCategoría = new System.Windows.Forms.Button();
+            this.lblResumen = new System.Windows.Forms.Label();
+            this.lblDatos3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblInfo1 = new System.Windows.Forms.Label();
+            this.lblInfo2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(706, 483);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(220, 60);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(646, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 290);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblRegistro
+            // 
+            this.lblRegistro.AutoSize = true;
+            this.lblRegistro.BackColor = System.Drawing.Color.Transparent;
+            this.lblRegistro.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegistro.ForeColor = System.Drawing.Color.White;
+            this.lblRegistro.Location = new System.Drawing.Point(21, 150);
+            this.lblRegistro.Name = "lblRegistro";
+            this.lblRegistro.Size = new System.Drawing.Size(136, 21);
+            this.lblRegistro.TabIndex = 2;
+            this.lblRegistro.Text = "Registro de Datos";
+            // 
+            // lblDatos1
+            // 
+            this.lblDatos1.AutoSize = true;
+            this.lblDatos1.BackColor = System.Drawing.Color.Transparent;
+            this.lblDatos1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatos1.ForeColor = System.Drawing.Color.White;
+            this.lblDatos1.Location = new System.Drawing.Point(21, 171);
+            this.lblDatos1.Name = "lblDatos1";
+            this.lblDatos1.Size = new System.Drawing.Size(431, 21);
+            this.lblDatos1.TabIndex = 3;
+            this.lblDatos1.Text = "Agregue o actualice todos los datos de sus gastos e ingresos.";
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistro.Location = new System.Drawing.Point(25, 195);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(220, 60);
+            this.btnRegistro.TabIndex = 4;
+            this.btnRegistro.Text = "Registrar Datos";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            // 
+            // lblCategoría
+            // 
+            this.lblCategoría.AutoSize = true;
+            this.lblCategoría.BackColor = System.Drawing.Color.Transparent;
+            this.lblCategoría.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoría.ForeColor = System.Drawing.Color.White;
+            this.lblCategoría.Location = new System.Drawing.Point(21, 298);
+            this.lblCategoría.Name = "lblCategoría";
+            this.lblCategoría.Size = new System.Drawing.Size(77, 21);
+            this.lblCategoría.TabIndex = 5;
+            this.lblCategoría.Text = "Categoría";
+            // 
+            // lblDatos2
+            // 
+            this.lblDatos2.AutoSize = true;
+            this.lblDatos2.BackColor = System.Drawing.Color.Transparent;
+            this.lblDatos2.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatos2.ForeColor = System.Drawing.Color.White;
+            this.lblDatos2.Location = new System.Drawing.Point(21, 319);
+            this.lblDatos2.Name = "lblDatos2";
+            this.lblDatos2.Size = new System.Drawing.Size(574, 21);
+            this.lblDatos2.TabIndex = 6;
+            this.lblDatos2.Text = "Observe el porcentaje de sus gastos y asigne un presupuesto para cada categoría.";
+            // 
+            // btnCategoría
+            // 
+            this.btnCategoría.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCategoría.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategoría.Location = new System.Drawing.Point(25, 343);
+            this.btnCategoría.Name = "btnCategoría";
+            this.btnCategoría.Size = new System.Drawing.Size(220, 60);
+            this.btnCategoría.TabIndex = 7;
+            this.btnCategoría.Text = "Ver Categorías";
+            this.btnCategoría.UseVisualStyleBackColor = false;
+            // 
+            // lblResumen
+            // 
+            this.lblResumen.AutoSize = true;
+            this.lblResumen.BackColor = System.Drawing.Color.Transparent;
+            this.lblResumen.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResumen.ForeColor = System.Drawing.Color.White;
+            this.lblResumen.Location = new System.Drawing.Point(21, 438);
+            this.lblResumen.Name = "lblResumen";
+            this.lblResumen.Size = new System.Drawing.Size(154, 21);
+            this.lblResumen.TabIndex = 8;
+            this.lblResumen.Text = "Resumen Financiero";
+            // 
+            // lblDatos3
+            // 
+            this.lblDatos3.AutoSize = true;
+            this.lblDatos3.BackColor = System.Drawing.Color.Transparent;
+            this.lblDatos3.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatos3.ForeColor = System.Drawing.Color.White;
+            this.lblDatos3.Location = new System.Drawing.Point(21, 459);
+            this.lblDatos3.Name = "lblDatos3";
+            this.lblDatos3.Size = new System.Drawing.Size(535, 21);
+            this.lblDatos3.TabIndex = 9;
+            this.lblDatos3.Text = "Generar y visualizar el resumen  de todos los datos ingresados y calculados.";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(25, 483);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 60);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Generar Resumen Financiero";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Navy;
+            this.lblTitulo.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(21, 26);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(203, 21);
+            this.lblTitulo.TabIndex = 11;
+            this.lblTitulo.Text = "¡Bienvenido a EasyFinance!";
+            // 
+            // lblInfo1
+            // 
+            this.lblInfo1.AutoSize = true;
+            this.lblInfo1.BackColor = System.Drawing.Color.Navy;
+            this.lblInfo1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo1.ForeColor = System.Drawing.Color.White;
+            this.lblInfo1.Location = new System.Drawing.Point(21, 47);
+            this.lblInfo1.Name = "lblInfo1";
+            this.lblInfo1.Size = new System.Drawing.Size(799, 21);
+            this.lblInfo1.TabIndex = 12;
+            this.lblInfo1.Text = "¡Bienvenido al programa definitivo del manejo de sus finanzas personales, donde p" +
+    "odrá registrar todos sus gastos";
+            // 
+            // lblInfo2
+            // 
+            this.lblInfo2.AutoSize = true;
+            this.lblInfo2.BackColor = System.Drawing.Color.Navy;
+            this.lblInfo2.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo2.ForeColor = System.Drawing.Color.White;
+            this.lblInfo2.Location = new System.Drawing.Point(21, 68);
+            this.lblInfo2.Name = "lblInfo2";
+            this.lblInfo2.Size = new System.Drawing.Size(526, 21);
+            this.lblInfo2.TabIndex = 13;
+            this.lblInfo2.Text = "e ingresos del mes para un mejor seguimiento de todas sus transacciones!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(980, 612);
+            this.Controls.Add(this.lblInfo2);
+            this.Controls.Add(this.lblInfo1);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblDatos3);
+            this.Controls.Add(this.lblResumen);
+            this.Controls.Add(this.btnCategoría);
+            this.Controls.Add(this.lblDatos2);
+            this.Controls.Add(this.lblCategoría);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.lblDatos1);
+            this.Controls.Add(this.lblRegistro);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSalir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EasyFinance";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblRegistro;
+        private System.Windows.Forms.Label lblDatos1;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.Label lblCategoría;
+        private System.Windows.Forms.Label lblDatos2;
+        private System.Windows.Forms.Button btnCategoría;
+        private System.Windows.Forms.Label lblResumen;
+        private System.Windows.Forms.Label lblDatos3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblInfo1;
+        private System.Windows.Forms.Label lblInfo2;
+    }
+}
+
